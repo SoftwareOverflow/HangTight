82437cb5fdd1ade44cc7f6ecddeb0a47c0556bc3
SoftwareOverflow <13178877+SoftwareOverflow@users.noreply.github.com>
2023-12-31 13:58:43 +0000

    Adding error checking for extra safety (#25)
    * Adding error checking for safety

--- app/src/main/java/com/softwareoverflow/hangtight/ui/history/write/HistorySaverLocal.kt
+++ app/src/main/java/com/softwareoverflow/hangtight/ui/history/write/HistorySaverLocal.kt
@@ -1,32 +1,45 @@
 package com.softwareoverflow.hangtight.ui.history.write
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.softwareoverflow.hangtight.data.WorkoutHistory
 import com.softwareoverflow.hangtight.ui.util.workout.WorkoutSection
 import java.time.LocalDate
 import javax.inject.Inject
 
-class HistorySaverLocal @Inject constructor (private val historyWriter: IHistoryWriter) : IHistorySaver {
+class HistorySaverLocal @Inject constructor(private val historyWriter: IHistoryWriter) :
+    IHistorySaver {
 
     var history = WorkoutHistory(0, 0, 0, LocalDate.now())
 
     override fun addHistory(seconds: Int, section: WorkoutSection) {
-        val dateNow = LocalDate.now()
+        try {
+            val dateNow = LocalDate.now()
 
-        if(history.date != dateNow)
-            write()
+            if (history.date != dateNow)
+                write()
 
-        when (section) {
-            WorkoutSection.Hang -> history.hangTime += seconds
-            WorkoutSection.Rest -> history.restTime += seconds
-            WorkoutSection.Recover -> history.recoverTime += seconds
-            else -> { /* Do Nothing */ }
+            when (section) {
+                WorkoutSection.Hang -> history.hangTime += seconds
+                WorkoutSection.Rest -> history.restTime += seconds
+                WorkoutSection.Recover -> history.recoverTime += seconds
+                else -> { /* Do Nothing */
+                }
+            }
+        } catch (e: Exception) {
+            // Blanket catch all - we don't want any problems here to interfere with the main app
+            FirebaseCrashlytics.getInstance().recordException(e)
         }
     }
 
     override fun write() {
-        historyWriter.writeHistory(history)
+        try {
+            historyWriter.writeHistory(history)
 
-        // Reset the history
-        history = WorkoutHistory(0, 0, 0, LocalDate.now())
+            // Reset the history
+            history = WorkoutHistory(0, 0, 0, LocalDate.now())
+        } catch (e: Exception) {
+            // Blanket catch all - we don't want any problems here to interfere with the main app
+            FirebaseCrashlytics.getInstance().recordException(e)
+        }
     }
 }
